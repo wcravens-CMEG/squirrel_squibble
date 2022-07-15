0de272def7d71796710f8d5ecd12897fe8a34ff8
Wes Cravens <wcravens@cortex-it.com>
2022-07-15 12:16:24 -0500

    Built a Segment List compnent.

---
+++ Browser_Client/src/features/segments/SegmentList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export const SegmentList = () {
+  const segments = useSelector(state => state.segments)
+
+  const renderedSegments = segments.map( segment => (
+    <article className="segment-excerpt" key={segment.id}>
+      <h3>{segment.title}</h3>
+      <p className="segment-content">{segment.content.substring(0, 100)}</p>
+    </article>
+  ))
+  return (
+    <section className="segments-list">
+      <h2>Section</h2>
+      {renderedSegments}
+    </section>
+  )
+}
